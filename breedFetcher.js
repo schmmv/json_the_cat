18d7c0fca4a6fe26bc3cd7f9e3f66c81ac42380d
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-16 20:37:46 +0000

    Handle edge case: breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const breed = process.argv.slice(2)[0];
+const breed = process.argv.slice(2);
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(url, (error, response, body) => {
@@ -8,5 +8,9 @@ request(url, (error, response, body) => {
     return;
   }
   const data = JSON.parse(body)[0];
+  if (!data) {
+    console.log("Requested breed is not found");
+    return;
+  }
   console.log(data.description);
 })
