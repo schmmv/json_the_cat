3350dda26f7023cbff53348967219c5887d0d5d9
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-16 20:44:31 +0000

    Refactor into a function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const breed = process.argv.slice(2);
-const url = `https://api.thecatapi.cm/v1/breeds/search?q=${breed}`;
+// const breed = process.argv.slice(2);
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('Request error: ', error);
-    return;
-  }
-  const data = JSON.parse(body)[0];
-  if (!data) {
-    console.log("Requested breed is not found");
-    return;
-  }
-  console.log(data.description);
-})
+const requestBreedInfo = function(breed) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('Request error: ', error);
+      return;
+    }
+    const data = JSON.parse(body)[0];
+    if (!data) {
+      console.log("Requested breed is not found");
+      return;
+    }
+    console.log(data.description);
+  });
+};
+
+requestBreedInfo(process.argv.slice(2));
