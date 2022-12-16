c464cfc34157b3808a647ff581e2bf11b0ac8a42
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-16 20:40:27 +0000

    Add 'Request error' to error console.log

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,10 @@
 const request = require('request');
 const breed = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const url = `https://api.thecatapi.cm/v1/breeds/search?q=${breed}`;
 
 request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.log('Request error: ', error);
     return;
   }
   const data = JSON.parse(body)[0];
